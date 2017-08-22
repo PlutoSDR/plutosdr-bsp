a13c8d12e075e801bb96d9741c7d7acef5b9ce97
jiangwei <jiangwei0402@gmail.com>
2017-08-22 18:17:03 +0800

    add soapysdr support

---
+++ packages/soapysdr/soapysdr.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# soapysdr
+#
+################################################################################
+SOAPYSDR_VERSION = 0.6.0
+SOAPYSDR_SOURCE = soapy-sdr-$(SOAPYSDR_VERSION).tar.gz
+SOAPYSDR_SITE = https://github.com/pothosware/SoapySDR/archive
+SOAPYSDR_INSTALL_STAGING = YES
+SOAPYSDR_LICENSE = Boost Software License 1.0
+SOAPYSDR_LICENSE_FILES = LICENSE_1_0.txt
+
+$(eval $(cmake-package))
