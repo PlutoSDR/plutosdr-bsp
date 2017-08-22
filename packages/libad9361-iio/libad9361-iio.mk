444079f05be5e42be11063e4be11d9aa24d7fd4c
jiangwei <jiangwei0402@gmail.com>
2017-08-22 13:40:32 +0800

    add libad9361-iio package

---
+++ packages/libad9361-iio/libad9361-iio.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# libad9361-iio
+#
+################################################################################
+LIBAD9361_IIO_VERSION = 6027674bc2ef400147f1607d0252e5347163f2f7
+LIBAD9361_IIO_SITE = https://github.com/analogdevicesinc/libad9361-iio
+LIBAD9361_IIO_SITE_METHOD = git
+
+LIBAD9361_IIO_INSTALL_STAGING = YES
+LIBAD9361_IIO_LICENSE = LGPL-2.1+
+LIBAD9361_IIO_LICENSE_FILE = LICENSE
+
+LIBAD9361_IIO__CONF_OPTS = -DCMAKE_CONFIGURATION_TYPES=Release
+
+$(eval $(cmake-package))
